4fcec651a21d33d34ab6d8f4b3e40ad6f5dbbad6
freyrads <frs18840@gmail.com>
2023-12-27 16:23:27 +0700

    add .env loader

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ZohoController } from './zoho/zoho.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController, ZohoController],
   providers: [AppService],
 })
