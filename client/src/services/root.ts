ed2f3227a45c43886fcdff2539eaadd476a73fe0
freyrads <frs18840@gmail.com>
2023-12-29 10:28:33 +0700

    enable cors

--- client/src/services/root.ts
+++ client/src/services/root.ts
@@ -1,3 +1,4 @@
+import { API_URL } from "@/config";
 import {
   ICreateDocumentParams,
   ICreateDocumentResponse,
@@ -10,19 +11,16 @@ import axios from "axios";
 export async function getUsers(): Promise<{
   data: IGetUsersResponse[];
 }> {
-  return axios.get("http://localhost:3001/users");
+  return axios.get(`${API_URL}/users`);
 }
 
 export async function getDocument(params: IGetDocumentParams) {
-  return axios.get<IGetDocumentResponse>("http://localhost:3001/zoho/preview", {
+  return axios.get<IGetDocumentResponse>(`${API_URL}/zoho/preview`, {
     params,
   });
 }
 export async function createDocument(params: ICreateDocumentParams) {
-  return axios.get<ICreateDocumentResponse>(
-    "http://localhost:3001/zoho/create",
-    {
-      params,
-    },
-  );
+  return axios.get<ICreateDocumentResponse>(`${API_URL}/zoho/create`, {
+    params,
+  });
 }
--- server/src/main.ts
+++ server/src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import * as zoho from './libs/zoho';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3001);
 
   //Initializing SDK once is enough. Calling here since code sample will be tested standalone.
