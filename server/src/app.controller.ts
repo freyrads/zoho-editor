a284c8af5b930701dcd21cb3597927e1c2f4927c
freyrads <frs18840@gmail.com>
2024-01-01 19:00:33 +0700

    forgot to add the actual endpoint

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,6 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { User as UserModel } from '@prisma/client';
+import {
+  User as UserModel,
+  Document as DocumentModel,
+  ZohoSession as ZohoSessionModel,
+} from '@prisma/client';
 
 @Controller()
 export class AppController {
@@ -9,5 +13,15 @@ export class AppController {
   @Get('users')
   getUsers(): Promise<UserModel[]> {
     return this.appService.getUsers();
+  }
+
+  @Get('documents')
+  getDocuments(): Promise<DocumentModel[]> {
+    return this.appService.getDocuments();
+  }
+
+  @Get('sessions')
+  getSessions(): Promise<ZohoSessionModel[]> {
+    return this.appService.getZohoSessions();
   }
 }
