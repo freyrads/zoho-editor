71a884ae157aba9d4ddae95cfe6accaa88b77147
freyrads <frs18840@gmail.com>
2024-01-03 10:10:57 +0700

    some adjustmen

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,16 @@ import * as zoho from './libs/zoho';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  await app.listen(
-    process.env.PORT?.length ? parseInt(process.env.PORT) : 3001,
-  );
+  const port = process.env.PORT;
+
+  await app.listen(port?.length ? parseInt(port) : 3001);
+
+  const srgp = process.env.SERVER_ROUTE_GLOBAL_PREFIX;
+
+  if (srgp?.length) {
+    console.log('Route global prefix:', srgp);
+    app.setGlobalPrefix(srgp);
+  }
 
   //Initializing SDK once is enough. Calling here since code sample will be tested standalone.
   //You can place SDK initializer code in your application and call once while your application start-up.
