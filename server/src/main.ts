bddf62d23c4146333520ee62a8f924639327f16e
freyrads <frs18840@gmail.com>
2024-01-03 10:16:14 +0700

    try fix not working global prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,9 +4,6 @@ import * as zoho from './libs/zoho';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  const port = process.env.PORT;
-
-  await app.listen(port?.length ? parseInt(port) : 3001);
 
   const srgp = process.env.SERVER_ROUTE_GLOBAL_PREFIX;
 
@@ -14,6 +11,10 @@ async function bootstrap() {
     console.log('Route global prefix:', srgp);
     app.setGlobalPrefix(srgp);
   }
+
+  const port = process.env.PORT;
+
+  await app.listen(port?.length ? parseInt(port) : 3001);
 
   //Initializing SDK once is enough. Calling here since code sample will be tested standalone.
   //You can place SDK initializer code in your application and call once while your application start-up.
