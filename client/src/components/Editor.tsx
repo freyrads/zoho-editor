9acea0150d444662fa84deedeb8e515fbc1beeab
freyrads <frs18840@gmail.com>
2024-01-09 09:39:17 +0700

    initial autosave

--- client/src/components/Editor.tsx
+++ client/src/components/Editor.tsx
@@ -2,7 +2,7 @@
 
 import { IEditorProps } from "@/interfaces/components";
 import { inspect } from "util";
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function Editor({
   data,
@@ -13,47 +13,16 @@ export function Editor({
 }: Readonly<IEditorProps>) {
   // min-w-screen max-w-screen is not working
   //
+  const [autosaveTimeout, setAutosaveTimeout] = useState(0);
+  const shouldAutosaveRef = useRef(false);
+  const autosaveTimerRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
   const editorId = `zoho-editor-${id}`;
 
   const hideSaveManuallyButton = saveButtonOptions.hide;
 
-  useEffect(() => {
-    const xdc = (window as any).XDC;
-    console.log({ xdc });
-
-    if (!xdc) return;
-    const iFrame = document.getElementById(editorId) as HTMLIFrameElement;
-
-    (window as any).XDC.setTarget({
-      origin: "https://api.office-integrator.com",
-      window: iFrame?.contentWindow,
-    });
-
-    const { isSheet } = saveButtonOptions;
-
-    if (isSheet) {
-      (window as any).XDC.receiveMessage(
-        "SaveSpreadsheetResponse",
-        function (data: any) {
-          console.log({ SaveSpreadsheetResponse: data });
-          onSave?.(data, { type: "sheet" });
-        },
-      );
-
-      return;
-    }
-
-    (window as any).XDC.receiveMessage(
-      "SaveDocumentResponse",
-      function (data: any) {
-        console.log({ SaveDocumentResponse: data });
-        onSave?.(data, { type: "writer" });
-      },
-    );
-  }, []);
-
-  const handleSaveManually = () => {
-    if (hideSaveManuallyButton || !(window as any).XDC) return;
+  const saveDocument = () => {
+    if (!(window as any).XDC) return;
 
     const { onSaveError, isSheet } = saveButtonOptions;
 
@@ -70,7 +39,7 @@ export function Editor({
       return;
     }
 
-    const { hideSaveButton, forceSave, /*saveUrlParams,*/ format } =
+    const { hideSaveButton, forceSave /*saveUrlParams, format*/ } =
       saveButtonOptions;
 
     (window as any).XDC.postMessage({
@@ -80,7 +49,7 @@ export function Editor({
           typeof hideSaveButton === "boolean" ? hideSaveButton : false, // Default value will be true
         forceSave: typeof forceSave === "boolean" ? forceSave : true, // Default value will be true
         // saveUrlParams: saveUrlParams,
-        format: format,
+        // format: format,
       },
       // Use "SaveDocumentResponse" event for oncomplete
       onexception: function (data: any) {
@@ -91,10 +60,108 @@ export function Editor({
     });
   };
 
+  const autosaveTimerEnd = () => {
+    if (!shouldAutosaveRef.current) return;
+
+    saveDocument();
+
+    shouldAutosaveRef.current = false;
+  };
+
+  const updateAutosaveSecondTimer = () => {
+    if (autosaveTimerRef.current) {
+      clearTimeout(autosaveTimerRef.current);
+      autosaveTimerRef.current = null;
+    }
+
+    if (autosaveTimeout <= 0) return autosaveTimerEnd();
+
+    setAutosaveTimeout((v) => --v);
+    autosaveTimerRef.current = setTimeout(updateAutosaveSecondTimer, 1000);
+  };
+
+  const triggerAutosaveCountdown = () => {
+    if (autosaveTimerRef.current) {
+      clearTimeout(autosaveTimerRef.current);
+      autosaveTimerRef.current = null;
+    }
+
+    setAutosaveTimeout(30);
+    autosaveTimerRef.current = setTimeout(updateAutosaveSecondTimer, 1000);
+
+    shouldAutosaveRef.current = true;
+  };
+
+  useEffect(() => {
+    const xdc = (window as any).XDC;
+    console.log({ xdc });
+
+    if (!xdc) return;
+    const iFrame = document.getElementById(editorId) as HTMLIFrameElement;
+
+    (window as any).XDC.setTarget({
+      origin: "https://api.office-integrator.com",
+      window: iFrame?.contentWindow,
+    });
+
+    (window as any).XDC.receiveMessage(
+      "SaveSpreadsheetResponse",
+      function (data: any) {
+        console.log({ SaveSpreadsheetResponse: data });
+        onSave?.(data, { type: "sheet" });
+      },
+    );
+
+    const { isSheet } = saveButtonOptions;
+
+    if (isSheet) {
+      (window as any).XDC.receiveMessage(
+        "SpreadsheetModified",
+        function (data: any) {
+          console.log({ SpreadsheetModified: data });
+          triggerAutosaveCountdown();
+        },
+      );
+
+      (window as any).XDC.receiveMessage(
+        "SaveSpreadsheetResponse",
+        function (data: any) {
+          console.log({ SaveSpreadsheetResponse: data });
+          onSave?.(data, { type: "sheet" });
+        },
+      );
+
+      return;
+    }
+
+    (window as any).XDC.receiveMessage(
+      "DocumentModified",
+      function (data: any) {
+        console.log({ DocumentModified: data });
+        triggerAutosaveCountdown();
+      },
+    );
+
+    (window as any).XDC.receiveMessage(
+      "SaveDocumentResponse",
+      function (data: any) {
+        console.log({ SaveDocumentResponse: data });
+        onSave?.(data, { type: "writer" });
+      },
+    );
+  }, []);
+
+  const handleSaveManually = () => {
+    if (hideSaveManuallyButton) return;
+
+    saveDocument();
+  };
+
   return (
     <main className="flex min-h-screen max-h-screen min-w-[100vw] max-w-[100vw] overflow-auto flex-col items-center">
       <div className="break-all">{data ? inspect(data) : "Loading..."}</div>
-      <div>
+      <div className="flex gap-[20px]">
+        <div>Saving automatically in: {autosaveTimeout} second.</div>
         {!hideSaveManuallyButton && (
           <button className="btn-look" onClick={handleSaveManually}>
             Save Manually
